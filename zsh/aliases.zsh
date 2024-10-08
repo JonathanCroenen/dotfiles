add434735e5492d33f26c499125b5151238ebf46
Jonathan Croenen <44034156+JonathanCroenen@users.noreply.github.com>
2024-10-08 10:19:11 +0200

    Update aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,10 +8,10 @@ alias vim='nvim'
 alias imgcat='wezterm imgcat'
 
 # ls
-alias ls='colorls --sd -A'
-alias lsa='colorls --sd -la'
-alias lsd='colorls --sd -d'
-alias lsf='colorls --sd -f'
+# alias ls='colorls --sd -A'
+# alias lsa='colorls --sd -la'
+# alias lsd='colorls --sd -d'
+# alias lsf='colorls --sd -f'
 
 # file operations
 alias cp='cp -iv'
