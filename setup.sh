1499c91ca1490970d7ec5bb93abc721fe18e94d7
Jonathan Croenen <jonathan.croenen@outlook.com>
2025-05-05 11:58:16 +0000

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-source zsh/.zshenv
+DOTFILES=$(pwd)
+XDG_CONFIG_HOME="$1"
 
 ln -s $DOTFILES/nvim $XDG_CONFIG_HOME/nvim
 
 ln -s $DOTFILES/zsh/.zshenv $HOME/.zshenv
 ln -s $DOTFILES/zsh/.zshrc $HOME/.zshrc
+ln -s $DOTFILES/zsh $XDG_CONFIG_HOME/zsh
 
 ln -s $DOTFILES/.wezterm.lua $HOME/.wezterm.lua
 ln -s $DOTFILES/.wezterm $HOME/.wezterm
+
+sudo chsh -s $USER
