d8bcf2a66f3479454442c44f2ec5ac94d22a2476
Jonathan Croenen <jonathan.croenen@outlook.com>
2025-09-02 09:25:03 +0200

    use sudo anyway because XDG_CONFIG_HOME doesnt seem to exist in root

--- setup.sh
+++ setup.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
-fi
-
 git submodule update --init
 
 OS=$(uname)
 if ! command -v zsh > /dev/null 2>&1
 then
   if [[ $OS == "Linux" ]]; then
-    apt install zsh -y
+    sudo apt install zsh -y
   elif [[ $OS == "Darwin" ]]; then
     brew install zsh
   fi
 fi
 
 DOTFILES=$(pwd)
-mkdir -p $XDG_CONFIG_HOME
+mkdir -p "$XDG_CONFIG_HOME"
 rm -rf $XDG_CONFIG_HOME/nvim $HOME/.zshenv $HOME/.zshrc $XDG_CONFIG_HOME/zsh $HOME/.wezterm.lua $HOME/.wezterm
 
 ln -s $DOTFILES/nvim $XDG_CONFIG_HOME/nvim
